a89c7f7fd3011b316fce115157267d0681a3bf04
joselito5550 <i42marmj@uco.es>
2016-05-11 13:11:29 +0200

    añadida searchVertex

--- practica4/includes/Grafo.hpp
+++ practica4/includes/Grafo.hpp
@@ -7,7 +7,7 @@ namespace ed{
         int **_matriz;
         bool _dirigido;
         
-        int _currVertices;
+        Vertice  *_currVertices;
         int _currLados;
         int _numVertices;
         int _numLados;         
@@ -30,7 +30,7 @@ namespace ed{
       bool isEmpty(){
           return getNumVertices()==0;
       }
-      
+      //DUDA!!!
       //Lo suyo es que devuelva un valor float (El valor numerico que corresponda)
       //Porque al aplicar los algoritmos de floyd nos es útil
       int adjacent(Vertice &v, Vertice &u){
@@ -45,6 +45,28 @@ namespace ed{
       
       void addVertex(Vertice &v){
           _v.push_back(v);
+          _numVertices++;
+      }
+      
+      //Hay que pasarle el peso para añadirlo a la matriz
+      void addEdge(Vertice &v, Vertice &u, int peso){
+          _matriz[v.getLabel()][u.getLabel()] = peso;
+          _matriz[u.getLabel()][v.getLabel()] = _matriz[v.getLabel()][u.getLabel()];
+      }
+      
+      //Buscamos el vertice v, si lo encontramos ponemos el cursor sobre éste
+      void searchVertex(Vertice &v){
+          bool encontrado = false;
+          int i = 0;
+          while(!encontrado && i<_numVertices){
+              if(_v[i] == v){
+                  encontrado = true;
+              }
+              else i++;
+          }
+          if(encontrado){
+              _currVertices = _v[i];
+          }
       }
      
     };
