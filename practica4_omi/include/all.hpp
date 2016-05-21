c540e8f622c54f075d8eda66d29b40d03c305006
Omar Sotillo Franco <kurodasogo@gmail.com>
2016-05-21 18:20:27 +0200

    Cambios en la organizacion de la P4

---
+++ practica4_omi/include/all.hpp
@@ -0,0 +1,17 @@
+/*!
+   \file all.hpp
+   \brief Including all libraries in the project.
+   \author Omar Sotillo Franco
+ */
+
+#ifndef __ALL_HPP_
+#define __ALL_HPP_
+
+#include "defines.hpp"
+#include "libraries.hpp"
+#include "../lib/interfaceDonor.hpp"
+#include "../lib/donor.hpp"
+#include "../lib/heap.hpp"
+#include "../lib/heapInterface.hpp"
+
+#endif // ifndef __ALL_HPP_
